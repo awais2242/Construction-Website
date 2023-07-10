da14c786f40673ade53a8c994c5614a509923562
Shafqat-Khan <shafqat333khan@gmail.com>
2023-07-10 22:32:10 +0500

    nav update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,62 +1,95 @@
-import React from "react";
+import { React, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch, faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSearch,
+  faCircleXmark,
+  faBars,
+} from "@fortawesome/free-solid-svg-icons";
 import logo from "../images/logo.png";
 
 import "./Navbar.css";
+import {
+  faFacebookF,
+  faInstagram,
+  faLinkedin,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Navbar = () => {
+  const [isContainerOpen, setIsContainerOpen] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsContainerOpen(true);
+  };
+  const handleCloseClick = () => {
+    setIsContainerOpen(false);
+  };
+
   return (
     <div>
       <div className="phone">Phone 818.351.3570</div>
       <div className="logo">
         <img src={logo} alt="logo"></img>
-        <div className="container"></div>
+        <div className="spacer"></div>
         <div className="social">
-          <FontAwesomeIcon icon={faSearch} />
-          <FontAwesomeIcon icon={faSearch} />
-          <FontAwesomeIcon icon={faSearch} />
-          <FontAwesomeIcon icon={faSearch} />
+          <FontAwesomeIcon className="facebook" icon={faFacebookF} />
+          <FontAwesomeIcon className="instagram" icon={faInstagram} />
+          <FontAwesomeIcon className="linkdin" icon={faLinkedin} />
+          <FontAwesomeIcon className="twitter" icon={faTwitter} />
         </div>
+        <FontAwesomeIcon
+          className="menu-button"
+          onClick={handleButtonClick}
+          icon={faBars}
+        />
       </div>
-      <div class="items">
-        <div class="item">
-          <div class="dropdown">
-            <a class="dropbtn" href="localhost:3000/">Home</a>
+      <div className={`container ${isContainerOpen ? "open" : ""}`}>
+        <div className="items">
+          <div className="item">
+            <div className="dropdown">
+              <a className="dropbtn" href="localhost:3000/">
+                Home
+              </a>
+            </div>
+            <div className="dropdown">
+              <a className="dropbtn" href="localhost:3000/">
+                About
+              </a>
+            </div>
+            <div className="dropdown">
+              <a className="dropbtn" href="localhost:3000/">
+                Services
+              </a>
+            </div>
+            <div className="dropdown">
+              <a className="dropbtn" href="localhost:3000/">
+                Pricing & Plans
+              </a>
+            </div>
+            <div className="dropdown">
+              <a className="dropbtn" href="localhost:3000/">
+                Example Projects
+              </a>
+            </div>
+            <div className="dropdown">
+              <a className="dropbtn" href="localhost:3000/">
+                Contact
+              </a>
+            </div>
+            <div className="dropdown">
+              <a className="dropbtn" href="localhost:3000/">
+                Request a Quote
+              </a>
+            </div>
           </div>
-          <div class="dropdown">
-            <a class="dropbtn" href="localhost:3000/">About</a>
-          </div>
-          <div class="dropdown">
-            <a class="dropbtn" href="localhost:3000/">Services</a>
-          </div>
-          <div class="dropdown">
-            <a class="dropbtn" href="localhost:3000/">Pricing & Plans</a>
-          </div>
-          <div class="dropdown">
-            <a class="dropbtn" href="localhost:3000/">Example Projects</a>
-          </div>
-          <div class="dropdown">
-            <a class="dropbtn" href="localhost:3000/">Contact</a>
-          </div>
-          <div class="dropdown">
-            <a class="dropbtn" href="localhost:3000/">Request a Quote</a>
-          </div>
-        </div>
-        <div className="search">
+          <div className="search">
             <FontAwesomeIcon icon={faSearch} />
           </div>
-        <div class="menu-icon">
-          <div class="menu-card">
-            <span></span>
-            <span></span>
-            <span></span>
+          <div className="close" onClick={handleCloseClick}>
+            <FontAwesomeIcon icon={faCircleXmark} />
           </div>
         </div>
-        <div className="close">
-        <FontAwesomeIcon icon={faCircleXmark} /> 
-        </div>
-             </div>
+      </div>
     </div>
   );
 };
